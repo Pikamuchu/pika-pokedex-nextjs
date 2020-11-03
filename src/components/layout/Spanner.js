bbf3c7b6364d9d919480b824e1f4b3860cfef8f1
Antonio Marin Jimenez <antonio.marin.jimenez@gmail.com>
2020-11-03 17:19:42 +0100

    Fix loading snap to not overlay footer #8

--- src/components/layout/Spanner.js
+++ src/components/layout/Spanner.js
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
-import { Spinner } from 'react-bootstrap';
 import { withTranslation } from '../../i18n';
 
 const Spanner = ({ t }) => {
   return (
     <div className="spanner d-flex flex-row-reverse">
       <div className="spinner-box">
-        <Spinner animation="grow" />
-        <p>{t('spinner-loading')}</p>
+        <svg className="spinner" width="30px" height="30px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+          <circle className="spinner-path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30" />
+        </svg>
+        <p className="spinner-text">{t('spinner-loading')}</p>
       </div>
     </div>
   );
@@ -17,4 +18,4 @@ Spanner.propTypes = {
   t: PropTypes.func.isRequired,
 };
 
-export default withTranslation('pokemon')(Spanner);
+export default withTranslation('common')(Spanner);
